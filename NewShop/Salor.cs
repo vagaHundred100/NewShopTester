8fad9097f32277b8dac93f6763512d4469812a8a
vagaHundred100 <53252321+vagaHundred100@users.noreply.github.com>
2020-03-03 00:30:39 +0400

    Update Salor.cs

--- NewShop/Salor.cs
+++ NewShop/Salor.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +8,9 @@ namespace NewShop
 {
     interface Salor
     {
-        Item Sell(Buyer buyer, string itemName);
+        Item Sell(string itemName,float price);
+
+        float GetItemPrice(string itemName);
         void Display();
     }
 }
