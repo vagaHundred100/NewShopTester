7b03c4e2cce125d552039f9dd9782fb6e58b83f4
vagaHundred100 <53252321+vagaHundred100@users.noreply.github.com>
2020-03-03 00:29:40 +0400

    Update Buyer.cs

--- NewShop/Buyer.cs
+++ NewShop/Buyer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +8,8 @@ namespace NewShop
 {
     interface Buyer
     {
-        void Buy(Salor salor, string itemName);
-        float PayForItem(float price);
+        void Add(Item item);
+        bool PayForItem(float price);
         void Display();
     }
 }
